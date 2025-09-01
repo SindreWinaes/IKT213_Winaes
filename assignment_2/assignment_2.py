2561040d728719cc33b1f6e377b19b8c8dce3514
Sindre Winæs <sindrew@uia.no>
2025-09-01 22:47:20 +0200

    added the start of assignment 2

---
+++ assignment_2/assignment_2.py
@@ -0,0 +1,33 @@
+
+def main():
+
+    print("this is main")
+
+
+def padding(image, border_width):
+    print("this is padding")
+
+
+def cropping():
+    print("this is cropping")
+
+def resizing():
+    print("this is resize")
+
+def copying():
+    print("this is copuing")
+
+def grayscale():
+    print("this is grayscale")
+
+def hue_shift():
+    print("this is hue-shift")
+
+def HSV():
+    print("this is HSV")
+
+def smoothing():
+    print("this is smoothing")
+
+def rotating():
+    print("this is rotating")
